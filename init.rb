4cba3b321822ba7213d37cc0ac5e9e51fae10396
Oleg Romeyko <oleg.romeyko@gmail.com>
2015-05-06 19:03:12 +0300

    Add locales, and rewrite init.rb

--- init.rb
+++ init.rb
@@ -1,3 +1,8 @@
+require 'redmine'
+require 'redmine_chilli_pdf'
+require 'wicked_pdf' unless defined?(WickedPdf)
+require 'nokogiri' unless defined?(Nokogiri)
+
 Redmine::Plugin.register :redmine_chilli_pdf do
   name 'Redmine Chilli Pdf plugin'
   author "//Twinslash"
@@ -5,4 +10,15 @@ Redmine::Plugin.register :redmine_chilli_pdf do
   version '0.0.1'
   url 'https://github.com/twinslash/redmine_chilli_pdf'
   author_url 'http://twinslash.com'
+
+  settings partial: 'settings/redmine_chilli_pdf_settings',
+           default: RedmineChilliPDF::Config.defaults
+end
+
+Rails.configuration.to_prepare do
+  require_dependency 'principal'
+  require_dependency 'user'
+  User.send(:include, UserPatch) unless User.included_modules.include? UserPatch
+
+  require_dependency 'wiki_format_hook_listener'
 end
