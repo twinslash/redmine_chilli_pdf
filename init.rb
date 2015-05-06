da17a5c5d3a4ca9e061517a465d20ecc31865035
Oleg Romeyko <oleg.romeyko@gmail.com>
2015-05-06 13:44:33 +0300

    Add plugin info

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :redmine_chilli_pdf do
   name 'Redmine Chilli Pdf plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author "//Twinslash"
+  description 'Plugin to export wiki in pdf'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/twinslash/redmine_chilli_pdf'
+  author_url 'http://twinslash.com'
 end
